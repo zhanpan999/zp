748b2e17d34944cdc83e43ac869048a0220831b4
zhanpan999 <70739006+zhanpan999@users.noreply.github.com>
2020-09-04 14:40:49 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,32 @@
+import com.alibaba.fastjson.JSON;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/test")
+public class test extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("1111");
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("test","test");
+        String json=JSON.toJSONString(map);
+        PrintWriter out=resp.getWriter();
+        out.print(json);
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+    }
+}
